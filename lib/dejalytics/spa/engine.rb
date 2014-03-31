0e3c8a3e51d6959301b743c7c9218093d23352e1
Juergen Walter <juwalter@gmail.com>
2014-03-31 23:16:15 +0200

    Adds fonts to asset precompile

--- lib/dejalytics/spa/engine.rb
+++ lib/dejalytics/spa/engine.rb
@@ -4,6 +4,11 @@ module Dejalytics
       isolate_namespace Dejalytics::SPA
       initializer 'dejalytics-spa.assets.precompile' do |app|
         app.config.assets.precompile += %w( vendor/require.js dejalytics-spa.js dejalytics-spa.css )
+        config.assets.precompile << Proc.new { |path|
+          if path =~ /\.(eot|svg|ttf|woff)\z/
+            true
+          end
+        }
       end
     end
   end
